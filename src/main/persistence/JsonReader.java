af21db6792fb6e052671b1bc675065335b8b583a
Wentao Gu <wgu06@student.ubc.ca>
2026-07-19 16:03:44 -0700

    reading/writing class implementation

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,11 +1,16 @@
 package persistence;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 import org.json.JSONObject;
 import org.json.JSONArray;
 
 import model.Player;
 import model.Event;
+import model.EventLibrary;
 
 public class JsonReader {
     private String source;
@@ -17,49 +22,92 @@ public class JsonReader {
     }
 
     /**
-     * effects: read file and create rebuild player, integrate all player's attribute, and events.
+     * effects: read file and create rebuild player, integrate all player's
+     * attribute, and events.
+     * 
      * @param filename path of the file to read
      * @return the player reconstructed from the saved file
      * @throws IOException if an error occurs while reading the file
      */
-    public Player loadGame(String filename) throws IOException {
-        return p1;
+    public Player loadGame(String filename, EventLibrary eventLibrary) throws IOException {
+        String jsonData = readFile(filename);
+        JSONObject gameJson = new JSONObject(jsonData);
+        JSONObject playerJson = gameJson.getJSONObject("Player");
+        Player player = loadPlayer(playerJson);
+        loadEvents(eventLibrary, gameJson);
+        return player;
     }
 
     /**
      * read json file and convert into String.
+     * 
      * @param filename path of the file to read
      * @return contents of the file as a string
      * @throws IOException if an error occurs while reading the file
      */
     private String readFile(String filename) throws IOException {
-        return null;
+        return Files.readString(Path.of(filename));
     }
 
     /**
      * effects: read player attributes.
      */
     private Player loadPlayer(JSONObject jsonObject) {
-        return null;
+        String name = jsonObject.getString("Name");
+        String birthplace = jsonObject.getString("Birthplace");
+        int age = jsonObject.getInt("Age");
+        int wisdom = jsonObject.getInt("Wisdom");
+        int san = jsonObject.getInt("San");
+        int mood = jsonObject.getInt("Mood");
+        Player player = new Player(name, birthplace, age);
+        player.setWisdom(wisdom);
+        player.setPlayerMood(mood);
+        player.setPlayerSan(san);
+        player.achievementMade();
+        return player;
     }
 
     /**
-     * modifies: Player p1.
+     * modifies: eventLibrary
      * effects loading all events and add to player object
-     * @param player player to which the events are added
-     * @param jsonObject JSON object containing the saved events
+     * 
+     * @param eventLibrary eventLibrary to which the events are added
+     * @param jsonObject   object containing the saved events
      */
-    private void loadEvents(Player player, JSONObject jsonObject){
-        
+    private void loadEvents(EventLibrary eventLibrary, JSONObject jsonObject) {
+        JSONArray events = jsonObject.getJSONArray("Gone Through Events");
+        for (int i = 0; i < events.length(); i++) {
+            JSONObject eventJson = events.getJSONObject(i);
+            Event event = loadEvent(eventJson);
+            eventLibrary.addGoneThroughEventInEvent(event);
+        }
     }
 
     /**
      * modifies: event
      * effects: add single events
+     * 
      * @param eventJson JSON object containing event information
      * @return event constructed from the JSON data
      */
     private Event loadEvent(JSONObject eventJson) {
-        return null;
+        String eventDescription = eventJson.getString("evenrDescription");
+        int minAge = eventJson.getInt("minAge");
+        int maxAge = eventJson.getInt("maxAge");
+        int sanChange = eventJson.getInt("sanChange");
+        int moodChange = eventJson.getInt("moodChange");
+        int wisdomChange = eventJson.getInt("wisdomChange");
+        String eventCode = eventJson.getString("eventCode");
+        JSONArray prerequistieJson = eventJson.getJSONArray("Prerequistie");
+        ArrayList<String> prerequisites = new ArrayList<>();
+        for (int i = 0; i < prerequistieJson.length(); i++) {
+            prerequisites.add(prerequistieJson.getString(i));
+        }
+        Event event = new Event(eventDescription, minAge, maxAge, sanChange, moodChange, wisdomChange, eventCode);
+        for (String p : prerequisites) {
+            event.addPrerequisite(p);
+        }
+
+        return event;
     }
 }
--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,8 +1,8 @@
 package persistence;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,34 +13,72 @@ import model.Event;
 
 public class JsonWriter {
 
+    private ArrayList<Event> goneThroughEvents;
+
     /**
      * modifies: this
-     * effects: save current player's enrolled events, all attributes into json format
+     * effects: save current player's enrolled events, all attributes into json
+     * format
      */
-    public void saveGame(Player p1, EventLibrary eventLibrary, String filename) throws IOException { 
+    public void saveGame(Player player, EventLibrary eventLibrary, String filename) throws IOException {
+        JSONObject json = new JSONObject();
+        json.put("Player", playerToJson(player));
+        json.put("Gone Through Events", eventsToJson(eventLibrary));
         try (PrintWriter printWriter = new PrintWriter(filename)) {
-            
-        } catch (Exception e) {
-           
+            printWriter.print(json.toString(4));
         }
     }
 
     /**
      * effects: transfor player data into json format
+     * 
      * @param player the player to convert
      * @return a JSONObject representing the player
      */
     private JSONObject playerToJson(Player player) {
-        return null;
+        JSONObject json = new JSONObject();
+        json.put("Name", player.getPlayerName());
+        json.put("Birthplace", player.getLocation());
+        json.put("Age", player.getPlayerAge());
+        json.put("Wisdom", player.getWisdom());
+        json.put("San", player.getPlayerSan());
+        json.put("Mood", player.getPlayerMood());
+
+        JSONArray achievements = new JSONArray();
+        for (String achievement : player.achievementMade()) {
+            achievements.put(achievement);
+        }
+        json.put("AchievementMade",achievements);
+        return json;
     }
 
     /**
      * effects: all listing events into json format
+     * 
      * @param eventLibrary the event library containing completed events
      * @return a JSONArray containing all completed events
      */
     private JSONArray eventsToJson(EventLibrary eventLibrary) {
-        return null;
+        JSONObject json = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        
+        goneThroughEvents = eventLibrary.goneThroughEventsInEvent();
+
+        for (Event eachEvent : goneThroughEvents) {
+            json.put("evenrDescription",eachEvent.getEventDescription());
+            json.put("minAge", eachEvent.getMinAge());
+            json.put("maxAge", eachEvent.getMaxAge());
+            json.put("sanChange", eachEvent.getSanChange());
+            json.put("moodChange", eachEvent.getMoodChange());
+            json.put("wisdomChange", eachEvent.getWisdomChange());
+            json.put("eventCode", eachEvent.getEventCode());
+            JSONArray jsonArrayPrerequistie = new JSONArray();
+            for (String prerequisites : eachEvent.getPrerequisites()) {
+                jsonArrayPrerequistie.put(prerequisites);
+            }
+            json.put("Prerequistie",jsonArrayPrerequistie);
+        }
+        return jsonArray.put(json);
     }
 
 }
